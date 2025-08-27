1380b07adc13ff41b938bfcc7820cd6b479e5c07
Matthew Hengl <matthengl21@gmail.com>
2025-08-27 15:14:39 -0700

    Updated .gitignore as well as made the new repo for the new daily challenge

---
+++ Simple Task SchedulerAndReminder System/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskScheduler;
+
+class TaskScheduler
+{
+    public void Main(string[] args)
+    {
+        //Read/Write to a JSON file
+            //Read
+                //Once the JSON is read in, it will display the closest and the highest priority of tasks in the list
+                //Once it reads the tasks in, it will delete any tasks that are past due
+            //Write
+                //The user can input another task
+                    //Date
+                    //Time
+                    //Priority
+                    //Category
+                //Once the user puts the task in, it'll add to the JSON file at the end
+    }
+}
